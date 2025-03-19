8486e35c954646ce789c984da292c73b534ae144
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-19 12:36:20 +0500

    adding files

---
+++ src/main/java/com/example/designpatterns/DesignPatternsDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsDemoApplication.class, args);
+    }
+}
